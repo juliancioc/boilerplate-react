18c064e636c0dca8ba976a54f243c8be9b6f8ddb
Juliâncio Carvalho <juliancio_alves@hotmail.com>
2021-10-02 16:13:06 -0300

    chore: prettier

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const Home = () => {
+  return (
+    <S.Wrapper>
+      <h1>Home</h1>
+    </S.Wrapper>
+  )
+}
+
+export default Home
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,13 @@
 import React from "react"
 import { Switch } from "react-router-dom"
+import Home from "../pages/Home"
 
 import Route from "./Route"
 
-const Routes: React.FC = () => <Switch></Switch>
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Home} />
+  </Switch>
+)
 
 export default Routes
